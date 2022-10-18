a6c4e70002fc8003393b7dd0d2378b4b199ee9df
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-18 19:49:45 +0200

    configure swagger

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import morgan from "morgan";
 import { Low, JSONFileSync } from "lowdb";
 
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 import booksRouter from "./routes/books.js";
 
 const PORT = process.env.PORT || 4000;
@@ -12,7 +14,31 @@ const db = new Low(adapter);
 
 db.data = db.data || { books: [] };
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Books Library",
+      version: "1.0.0",
+      description: "Simple Books Library",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+      {
+        url: "https://google.com",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
+
 const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.db = db;
 
