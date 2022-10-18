7c85ed2970e2955eac7eedda9400cc61be170a30
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-18 20:01:58 +0200

    adding a schema

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,32 @@ const router = express.Router();
 
 const IDLENGTH = 8;
 
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *    Book:
+ *     type: object
+ *     required:
+ *      - title
+ *      - author
+ *     properties:
+ *      id:
+ *       type: string
+ *       description: the unique id of the book
+ *      title:
+ *       type: string
+ *       description: the title of the book
+ *      author:
+ *       type: string
+ *       description: the author of the book
+ *     example:
+ *      id: d5e-azd
+ *      title: Cool Book
+ *      author: Zeyad Tarek
+ *
+ */
+
 router.get("/", async (req, res) => {
   await req.app.db.read();
   const books = req.app.db.data.books;
