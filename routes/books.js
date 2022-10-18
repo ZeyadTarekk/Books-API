16843061441176907b04f2cec409aa62cb994a49
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-18 19:37:34 +0200

    delete a book

--- routes/books.js
+++ routes/books.js
@@ -4,27 +4,46 @@ const router = express.Router();
 
 const IDLENGTH = 8;
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  await req.app.db.read();
   const books = req.app.db.data.books;
   console.log(books);
   res.json(books);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
+  await req.app.db.read();
   const book = req.app.db.data.books.find((el) => el.id === req.params.id);
   res.send(book);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const book = {
       id: nanoid(IDLENGTH),
       ...req.body,
     };
     req.app.db.data.books.push(book);
+    await req.app.db.write();
     res.json(book);
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await req.app.db.read();
+    req.app.db.data.books = req.app.db.data.books.filter(
+      (el) => el.id !== req.params.id
+    );
+
+    await req.app.db.write();
+    res.json(req.app.db.data.books);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 export default router;
